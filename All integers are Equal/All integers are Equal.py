3fc1122f871ed8d6112f35d0f517a132bfc153e5
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2023-10-14 22:02:54 +0600

    All integers are Equal

--- All integers are Equal/All integers are Equal.py
+++ All integers are Equal/All integers are Equal.py
@@ -12,14 +12,20 @@ Input #2
 2 3 4 2 6
 Output
 No
+
+-------------------------
+| Prepared by -         |
+| Himel Sarder          |
+| Dept. of CSE, BSFMSTU |
+-------------------------
 """
 
-N = int(input())
-A = list(map(int, input().split()))
+N = int(input())    # 5
+A = list(map(int, input().split()))     # A = [2, 3, 4, 2, 6]
 
-all_equal = all(x == A[0] for x in A)
+all_equal = all(x == A[0] for x in A)  #Test Center
 
 if all_equal:
     print("Yes")
 else:
-    print("No")
+    print("No")   #Excecute
